b6b2258ac3719199b302e2a1cf2113e7d7d5f17c
Kisthanny <tigerqige@gmail.com>
2024-04-08 12:55:25 +0800

    frontend connect to blockchain and page mock up

--- src/App.js
+++ src/App.js
@@ -1,29 +1,65 @@
-import { useEffect, useState } from 'react';
-import { ethers } from 'ethers';
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
 
 // Components
-import Navigation from './components/Navigation';
-import Search from './components/Search';
-import Home from './components/Home';
+import Navigation from "./components/Navigation";
+import Search from "./components/Search";
+import Home from "./components/Home";
 
 // ABIs
-import RealEstate from './abis/RealEstate.json'
-import Escrow from './abis/Escrow.json'
+import RealEstate from "./abis/RealEstate.json";
+import Escrow from "./abis/Escrow.json";
 
 // Config
-import config from './config.json';
+import config from "./config.json";
 
 function App() {
+  const [account, setAccount] = useState(null);
+  const loadBlockchainData = async () => {
+    if (window.ethereum === undefined) {
+      throw new Error("please install Metamask");
+    }
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+    window.ethereum.on("accountsChanged", async () => {
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      const address = ethers.utils.getAddress(accounts[0]);
+      setAccount(address);
+    });
+  };
+
+  useEffect(() => {
+    loadBlockchainData();
+  }, []);
 
   return (
     <div>
-
-      <div className='cards__section'>
-
+      <Navigation account={account} setAccount={setAccount} />
+      <Search />
+      <div className="cards__section">
         <h3>Welcome to Millow</h3>
 
-      </div>
+        <hr />
 
+        <div className="cards">
+          <div className="card">
+            <div className="card__image">
+              <img src="" alt="Home" />
+            </div>
+            <div className="card__info">
+              <h4>1 ETH</h4>
+              <p>
+                <strong>1</strong> bds |
+                <strong>2</strong> ba |
+                <strong>3</strong> sqft
+              </p>
+              <p>1234 Elm St</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,48 @@
-import logo from '../assets/logo.svg';
+import logo from "../assets/logo.svg";
 
 const Navigation = ({ account, setAccount }) => {
+  const connectHandler = async () => {
+    if (account) {
+      return;
+    }
+    if (window.ethereum === undefined) {
+      throw new Error("please install Metamask");
+    }
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    setAccount(accounts[0]);
+  };
+  const formatAccount = () => {
+    if (!account) {
+      return "";
+    }
+    return `${account.slice(0, 6)}...${account.slice(-4)}`;
+  };
+  return (
+    <nav>
+      <ul className="nav__links">
+        <li>
+          <a href="#">Buy</a>
+        </li>
+        <li>
+          <a href="#">Rent</a>
+        </li>
+        <li>
+          <a href="#">Sell</a>
+        </li>
+      </ul>
 
-}
+      <div className="nav__brand">
+        <img src={logo} alt="Logo" />
+        <h1>Millow</h1>
+      </div>
+
+      <button type="button" className="nav__connect" onClick={connectHandler}>
+        {formatAccount() || "Connect"}
+      </button>
+    </nav>
+  );
+};
 
 export default Navigation;
